dc83a9c1e4cb523261667b3fc04162fd098f3618
Simon Hofmann <simon.hofmann@consol.de>
2019-05-17 10:39:13 +0200

    Initial commit

---
+++ sakuli-rollup-hooks/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function","128":"Class","2048":"Method"},"rows":[{"id":0,"kind":128,"name":"RollupLifecycleHooks","url":"classes/rolluplifecyclehooks.html","classes":"tsd-kind-class"},{"id":1,"kind":2048,"name":"readFileContent","url":"classes/rolluplifecyclehooks.html#readfilecontent","classes":"tsd-kind-method tsd-parent-kind-class","parent":"RollupLifecycleHooks"},{"id":2,"kind":2048,"name":"useRollup","url":"classes/rolluplifecyclehooks.html#userollup","classes":"tsd-kind-method tsd-parent-kind-class","parent":"RollupLifecycleHooks"},{"id":3,"kind":64,"name":"sakuliRollupPreset","url":"globals.html#sakulirolluppreset","classes":"tsd-kind-function"}]};
